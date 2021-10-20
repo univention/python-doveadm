612187c261678171969dac0ed60f0e902e2a62e2
Per Macha <macha@univention.de>
2021-10-21 00:00:00 +0200

    fix(clean-script): correct pycache cleanup
    - support non-alphanumeric names [SC2038]
    - add missing path [SC2185]

--- clean.sh
+++ clean.sh
@@ -3,4 +3,4 @@
 python3 setup.py clean --all
 rm -rf MANIFEST .coverage dist/doveadm* build/* ./*.egg-info .tox .eggs docs/.build/* .mypy_cache
 rm -f doveadm/*.py? tests/*.py? ./*.py?
-find -name __pycache__ | xargs -iname rm -r name
+find . -name __pycache__ -print0 | xargs --null --replace=name rm -r name
