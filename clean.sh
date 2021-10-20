81b51b0d3e0b4bf595df75f27236e8f98830b169
Per Macha <macha@univention.de>
2021-10-21 00:00:00 +0200

    fix(clean-script): protect against dash-globs [SC2035]

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 python3 setup.py clean --all
-rm -rf MANIFEST .coverage dist/doveadm* build/* *.egg-info .tox .eggs docs/.build/* .mypy_cache
-rm -f doveadm/*.py? tests/*.py? *.py?
+rm -rf MANIFEST .coverage dist/doveadm* build/* ./*.egg-info .tox .eggs docs/.build/* .mypy_cache
+rm -f doveadm/*.py? tests/*.py? ./*.py?
 find -name __pycache__ | xargs -iname rm -r name
